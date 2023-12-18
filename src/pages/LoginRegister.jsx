7777b22ca417732f25d14317b5f05d282f523e94
Bagas Mahda Dhani <muksinsaja12@gmail.com>
2023-12-18 15:24:13 +0700

    edit: add LoginForm and RegisterForm

--- src/pages/LoginRegister.jsx
+++ src/pages/LoginRegister.jsx
@@ -3,20 +3,43 @@ import LoginForm from "../feature/auth/components/LoginForm";
 import RegisterForm from "../feature/auth/components/RegisterForm";
 
 const LoginRegister = () => {
-  const [showForm] = useState(false);
+  const [showForm, setShowForm] = useState(false);
   return (
     <>
-      <div className="hero h-screen">
-        <div className="hero-content flex-col-reverse lg:flex-row-reverse">
-          <div className="w-1/2 flex justify-center">
-            {!showForm ? <LoginForm /> : <RegisterForm />}
-          </div>
-          <div className="text-center lg:text-left w-1/2">
+      <div className="hero">
+        <div className="hero-content flex-col lg:flex-row py-6">
+          <div className="text-center lg:self-start lg:text-left w-1/2 ">
             <h1 className="text-5xl font-bold">Si Obe</h1>
             <p className="py-6 ">
               si-OBE is a web-based application for the management of student
               grades based on the OBE (Outcome Based Education) system.
             </p>
+          </div>
+          <div className="w-1/2 flex flex-col items-center">
+            {!showForm ? <LoginForm /> : <RegisterForm />}
+            <div className="flex pt-2 gap-2">
+              {!showForm ? (
+                <>
+                  <p>Belum memiliki akun?</p>
+                  <span
+                    className="font-medium text-primary cursor-pointer"
+                    onClick={() => setShowForm((prev) => !prev)}
+                  >
+                    Register
+                  </span>
+                </>
+              ) : (
+                <>
+                  <p>Sudah memiliki akun?</p>
+                  <span
+                    className="font-medium text-primary cursor-pointer"
+                    onClick={() => setShowForm((prev) => !prev)}
+                  >
+                    Login
+                  </span>
+                </>
+              )}
+            </div>
           </div>
         </div>
       </div>
