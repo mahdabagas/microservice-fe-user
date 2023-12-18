3912ac4aed68bf97977a4d2bf275dc5e8c1cb843
Bagas Mahda Dhani <muksinsaja12@gmail.com>
2023-12-18 15:25:23 +0700

    edit: add route user edit dan add

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import DashboardUser from "./pages/DashboardUser";
 import LoginRegister from "./pages/LoginRegister";
+import EditUserForm from "./feature/users/components/EditUserForm";
+import RegisterForm from "./feature/auth/components/RegisterForm";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/login" element={<LoginRegister />} />
-        <Route path="/user" element={<DashboardUser />} />
+        <Route path="/" element={<LoginRegister />} />
+        <Route path="/users" element={<DashboardUser />} />
+        <Route path="/users/:id" element={<EditUserForm />} />
+        <Route path="/users/add" element={<RegisterForm />} />
       </Routes>
     </>
   );
